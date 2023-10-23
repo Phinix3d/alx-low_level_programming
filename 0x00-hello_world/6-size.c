30ea6f60aad5369c6deb083e47a3cf69b6b67d18
Phinix3d <saraahaik1@gmail.com>
2023-10-23 08:58:00 -0700

    lll

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,9 +9,9 @@
 
 int main(void)
 {
-	printf("Size of char: %lu bytes\n", sizeof(char));
+	printf("Size of char: %u bytes\n", sizeof(char));
 	printf("Size of int: %u bytes\n", sizeof(int));
-	printf("Size of long int: %lu bytes\n", sizeof(long int));
+	printf("Size of long int: %u bytes\n", sizeof(long int));
 	printf("Size of long long int: %u bytes\n", sizeof(long long int));
 	printf("Size of float: %u bytes\n", sizeof(float));
 	return (0);
