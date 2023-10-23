831502cc7dca3690eaf6b8a8dba6eabd83cc43a0
Phinix3d <saraahaik1@gmail.com>
2023-10-23 09:34:43 -0700

    hee

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+/**
+ * main -Entry point
+ *
+ * Description: print quote using write function
+ * write(int fd,const void *buf, size_t count)
+ *
+ * Return: 1 (not success)
+*/
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
