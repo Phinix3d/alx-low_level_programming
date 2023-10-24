5eda31436a4b7f71c275d6420d848f5634fac714
Phinix3d <saraahaik1@gmail.com>
2023-10-24 09:08:07 -0700

    nn

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,16 +10,13 @@
 
 int main(void)
 {
-	putchar('0' + 0);
-	putchar('0' + 1);
-	putchar('0' + 2);
-	putchar('0' + 3);
-	putchar('0' + 4);
-	putchar('0' + 5);
-	putchar('0' + 6);
-	putchar('0' + 7);
-	putchar('0' + 8);
-	putchar('0' + 9);
+	int number = 0;
+
+	while (number <= 9)
+	{
+		putchar(number + '0');
+		number++;
+	}
 	putchar('\n');
 	return (0);
 }
