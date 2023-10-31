5f16207241cd986d5d6df634ac7e9946d4530b8f
Phinix3d <saraahaik1@gmail.com>
2023-10-31 01:10:00 -0700

    allocate

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars,
+ * and initializes it with a specific char.
+ * @size: the size of the array
+ * @c: initial value
+ *
+ * Return: a pointer to the array, or NULL if it fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	char *originalPtr;
+
+	if (size == 0)
+		return (NULL);
+	ptr = (char *)malloc(size * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	originalPtr = ptr;
+
+	while (size > 0)
+		{
+			*ptr = c;
+			ptr++;
+			size--;
+		}
+	return (originalPtr);
+}
