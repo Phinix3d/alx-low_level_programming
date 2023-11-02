4f80cde00dcd50c16b1be3782def5d2a5a6ed9c7
Phinix3d <saraahaik1@gmail.com>
2023-11-02 00:50:07 -0700

    gege

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,42 +10,42 @@
 char **strtow(char *str)
 {
 	char **words;
-	int i, j, word_count = 0, len = 0;
+	int i, j, word_count = 0;
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] != ' ')
-		{
-			for (word_count++; str[i] != ' ' && str[i] != '\0'; len++)
-				i++;
-		}
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+			word_count++;
 	}
-	if (word_count == 0)
-		return (NULL);
 	words = (char **)malloc((word_count + 1) * sizeof(char *));
 	if (words == NULL)
 		return (NULL);
-	for (i = 0, j = 0; i < word_count; j++)
+	for (i = 0; *str != '\0'; i++)
 	{
-		while (str[j] == ' ')
-			j++;
-		for (len = 0; str[j] != ' ' && str[j] != '\0'; j++)
-			len++;
-		words[i] = (char *)malloc((len + 1) * sizeof(char));
-		if (words[i] == NULL)
+		if (*str != ' ')
 		{
-			for (i = 0; i < word_count; i++)
-				free(words[i]);
-			free(words);
-			return (NULL);
+			j = 0;
+			while (str[j] != ' ' && str[j] != '\0')
+				j++;
+
+			words[i] = (char *)malloc((j + 1) * sizeof(char));
+			if (words[i] == NULL)
+			{
+				while (i >= 0)
+					free(words[i--]);
+				free(words);
+				return (NULL);
+			}
+			for (j = 0; *str != ' ' && *str != '\0'; j++)
+				words[i][j] = *str++;
+			words[i][j] = '\0';
 		}
-		j -= len;
-		for (len = 0; str[j] != ' ' && str[j] != '\0'; len++, j++)
-			words[i][len] = str[j];
-		words[i][len] = '\0';
-		i++;
+		else
+		{
+			str++;
+		}
 	}
 	words[i] = NULL;
 	return (words);
