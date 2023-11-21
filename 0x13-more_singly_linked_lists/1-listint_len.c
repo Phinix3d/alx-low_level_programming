74ea48f5e062ded3971d8afe7c6efa1a43ed2cf3
Phinix3d <saraahaik1@gmail.com>
2023-11-21 09:13:28 -0800

    returning the number of elements

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,1 +1,20 @@
-#
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * listint_len - the number of elements
+ * @h: pointer to the head of the list
+ * Return: the number of elements in the linked list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
