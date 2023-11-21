fb350a28878123f5b0c2fd84b3c989ad3472188f
Phinix3d <saraahaik1@gmail.com>
2023-11-21 10:54:22 -0800

    fr2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint2 -frees list
+ * @head: pointer to the pointer of the head of list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	while (*head != NULL)
+	{
+		ptr = *head;
+		*head = (*head)->next;
+		free(ptr);
+	}
+}
