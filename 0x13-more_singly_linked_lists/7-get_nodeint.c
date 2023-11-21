a6e400f9fe1f285c22a80c57a86693c76b6fa355
Phinix3d <saraahaik1@gmail.com>
2023-11-21 11:59:04 -0800

    index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- * get_nodeint - returns the nth node of a listint_t linked list
+ * get_nodeint_at_index - returns the nth node of a listint_t linked list
  * @head: pointer to the head of the list
  * @index: index of the node, starting at 0
  * Return: pointer to the nth node, or
